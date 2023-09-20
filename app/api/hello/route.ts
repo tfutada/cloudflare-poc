f84cd80c9a4f61d991d68e8ae4a411ecba5c37f3
tfutada <tafutada@gmail.com>
2023-09-20 12:22:52 +0700

    init

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -6,5 +6,5 @@ import type { NextRequest } from 'next/server'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
+  return new Response(JSON.stringify({ name: 'John Doe123 456 9999' }))
 }
