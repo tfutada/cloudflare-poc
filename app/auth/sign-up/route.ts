95af1bd3c334f9dd661db666454464ca3a936858
tfutada <tafutada@gmail.com>
2023-09-21 12:48:34 +0700

    supabase auth

--- app/auth/sign-up/route.ts
+++ app/auth/sign-up/route.ts
@@ -1,41 +1,45 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import {createRouteHandlerClient} from '@supabase/auth-helpers-nextjs'
+import {cookies} from 'next/headers'
+import {NextResponse} from 'next/server'
 
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic'
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url)
-  const formData = await request.formData()
-  const email = String(formData.get('email'))
-  const password = String(formData.get('password'))
-  const supabase = createRouteHandlerClient({ cookies })
+    const requestUrl = new URL(request.url)
+    const formData = await request.formData()
+    const email = String(formData.get('email'))
+    const password = String(formData.get('password'))
+    const supabase = createRouteHandlerClient({cookies})
 
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${requestUrl.origin}/auth/callback`,
-    },
-  })
+    console.log('email', email)
+    console.log('password', password)
 
-  if (error) {
-    console.error(error)
+    const {error} = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            emailRedirectTo: `${requestUrl.origin}/auth/callback`,
+        },
+    })
+
+    if (error) {
+        console.error('error', error)
+
+        return NextResponse.redirect(
+            `${requestUrl.origin}/login?error=Could not authenticate user`,
+            {
+                // a 301 status is required to redirect from a POST to a GET route
+                status: 301,
+            }
+        )
+    }
 
     return NextResponse.redirect(
-      `${requestUrl.origin}/login?error=Could not authenticate user`,
-      {
-        // a 301 status is required to redirect from a POST to a GET route
-        status: 301,
-      }
+        `${requestUrl.origin}/login?message=Check email to continue sign in process`,
+        {
+            // a 301 status is required to redirect from a POST to a GET route
+            status: 301,
+        }
     )
-  }
-
-  return NextResponse.redirect(
-    `${requestUrl.origin}/login?message=Check email to continue sign in process`,
-    {
-      // a 301 status is required to redirect from a POST to a GET route
-      status: 301,
-    }
-  )
 }
